1c09dfd10c526ab3a19e7194444e7e62050ec464
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2020-02-25 04:12:04 -0500

    Add getAllExtensions function.
    Returns a std::set of extensions stored as std::strings. I'm using
    because it offers efficient insertions / insertion attempts O(log(n))
    and because it takes care of only having one of each element for me.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <filesystem>
 #include <iostream>
+#include <set>
+#include <string>
 
+std::set<std::string> getAllExtensions(std::filesystem::path toSearch)
+{
+	std::set<std::string> extensions; //using std::set for efficient insertions and because I'll never insert more than
+	                                  // 1 of the same extension
+	
+	for (std::filesystem::directory_entry dir : std::filesystem::recursive_directory_iterator(toSearch))
+	{
+		if (dir.is_regular_file())
+		{
+			extensions.insert(dir.path().extension().string());
+		}
+	}
 
-
+	return extensions;
+}
 
 int main()
 {

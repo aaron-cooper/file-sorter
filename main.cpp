8bbacb3a702ef7e02c8dfbb670cb539b4e041986
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2020-02-25 16:01:31 -0500

    Add comment to getAllExtensions.

--- main.cpp
+++ main.cpp
@@ -20,6 +20,9 @@ std::set<std::filesystem::path> getAllExtensions(std::filesystem::path toSearch)
 	//using std::set for efficient insertions and because I'll never insert more than
 	// 1 of the same extension
 	std::set<std::filesystem::path> extensions;	
+
+	//for each directory entry if it's a file with an extension attempt to add
+	//the extension to the set.
 	for (std::filesystem::directory_entry dir : std::filesystem::recursive_directory_iterator(toSearch))
 	{
 		if (dir.is_regular_file() && dir.path().has_extension())

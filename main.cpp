d4aa497808db7104d486c0fb1c834398e5a4d2d2
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2020-02-25 05:21:35 -0500

    Fix weird comment format.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 
 std::set<std::filesystem::path> getAllExtensions(std::filesystem::path toSearch)
 {
-	std::set<std::filesystem::path> extensions; //using std::set for efficient insertions and because I'll never insert more than
-	                                  // 1 of the same extension
-	
+	//using std::set for efficient insertions and because I'll never insert more than
+	// 1 of the same extension
+	std::set<std::filesystem::path> extensions;	
 	for (std::filesystem::directory_entry dir : std::filesystem::recursive_directory_iterator(toSearch))
 	{
 		if (dir.is_regular_file() && dir.path().has_extension())

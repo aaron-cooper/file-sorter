b402f3dccb556289ff914adb0ed810690bc0ad55
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2020-03-09 05:25:16 -0400

    Adjust main.cpp to use new class.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <filesystem>
 #include <iostream>
 #include <set>
-
-
+#include "FileSorter.hpp"
 
 int main()
 {
+	//right now the program relocate files from the current directory
+	//to outputFolder by copying. I'm going to let the user choose
+	//these values later via command line args, but that's not gonna
+	//happen until I have more time.
+	FileSorter sorter("./", "./outputFolder/", FileSorter::COPY);
 
-	std::set<std::filesystem::path> extensions;
+	if (sorter.isValid())
+	{
+		sorter.relocateFiles();
+	}
+	else
+	{
+		std::cout << "was not valid" << std::endl;
+	}
 
-	extensions = getAllExtensions(std::filesystem::current_path());
-	createSetDirectories("testExtFolder", extensions);
-	copyFilesToExtensionDirectories(std::filesystem::current_path(), "testExtFolder");
+	
 	return 0;
 }

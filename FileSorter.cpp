df94e6ac64d11afa1773704eb938e5a75f2aae42
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2020-03-09 03:47:30 -0400

    Add beginnings of FileSorter class.
    For now it's just a constructor and the members I'll need later.
    Methods are going to be added shortly.

--- FileSorter.cpp
+++ FileSorter.cpp
@@ -1,1 +1,8 @@
-#include <FileSorter.hpp>
+#include "FileSorter.hpp"
+
+
+FileSorter::FileSorter(std::filesystem::path toSearch, std::filesystem::path destDir, FileSorter::RelocationMode mode) : toSearch(toSearch), destDir(destDir), rmode(mode)
+{
+	return;
+}
+
--- FileSorter.hpp
+++ FileSorter.hpp
@@ -1,12 +1,19 @@
 #ifndef FILESORTER_H
 #define FILESORTER_H
 
+#include <filesystem>
 
 
 class FileSorter {
 public:
+	enum RelocationMode {MOVE, COPY};
+
+	FileSorter(std::filesystem::path toSearch, std::filesystem::path destDir, RelocationMode mode);
 
 private:
+	std::filesystem::path toSearch;
+	std::filesystem::path destDir;
+	RelocationMode rmode = COPY;
 
 };
 
